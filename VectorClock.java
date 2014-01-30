2a7c13819f6fd7709cbbf23131e94d24c6477fb1
Nikhil <nikhil@Nikhil-PC.(none)>
2014-01-30 03:34:17 -0800

    Please make all the func compatible to your messagepasser & message. Please check the code

--- VectorClock.java
+++ VectorClock.java
@@ -1,16 +1,15 @@
-
 public class VectorClock implements ClockService<int[], TimeStampedMessage> {
-	
-	private int id; // IDs can be assigned in MessagePasser after parsing the config file: alice 1, bob 2, charlie 3
+
+	private int id; // IDs can be assigned in MessagePasser after parsing the
+					// config file: alice 1, bob 2, charlie 3
 	private int[] timestamp;
-	
-	public VectorClock(int size, int id)
-    {
-              this.id = id;
-              /* Here, size = total number of users (Every user has his own timestamp) */
-              this.timestamp = new int[size];
-    }
-	  
+
+	public VectorClock(int size, int id) {
+		this.id = id;
+		/* Here, size = total number of users (Every user has his own timestamp) */
+		this.timestamp = new int[size];
+	}
+
 	@Override
 	public int[] increment() {
 		timestamp[id]++;
@@ -24,151 +23,54 @@ public class VectorClock implements ClockService<int[], TimeStampedMessage> {
 
 	@Override
 	public int compare(int[] message1, int[] message2) {
-		 
-		 int i;
-		 /* message 1 is less than or equal to message 2*/
-         for(i = 0; i < message1.length; i++)
-         {
-                 if(message1[i] <= message2[i])
-                         continue;
-                 break;
-         }
-         if(i == message1.length)
-                 return -1;
-         else
-                 return false;
-		
-		
-		 if(less_than(message1, message2))
-             return -1;
-     else if(less_than(message2, message1))
-             return 1;
-     else
-             return 0;
-	}
-	
-	   private boolean less_than_equal(int[] tm1, int[] tm2)
-       {
-               int i;
-               for(i = 0; i < tm1.length; i++)
-               {
-                       if(tm1[i] <= tm2[i])
-                               continue;
-                       break;
-               }
-               if(i == tm1.length)
-                       return true;
-               else
-                       return false;
-       }
 
-       private boolean less_than(int[] tm1, int[] tm2)
-       {
-               return (less_than_equal(tm1, tm2) && !equal(tm1, tm2));
-       }
-       
+		int i;
+		/* message 1 is less than or equal to message 2 */
+		for (i = 0; i < message1.length; i++) {
+			if (message1[i] <= message2[i])
+				continue;
+			break;
+		}
+		if (i == message1.length)
+			return -1;
+
+		/* message 2 is less than or equal to message 1 */
+		for (i = 0; i < message2.length; i++) {
+			if (message2[i] <= message1[i])
+				continue;
+			break;
+		}
+		if (i == message1.length)
+			return 1;
+
+		/* message1 == message2 */
+		return 0;
+	}
 
 	@Override
 	public void clockSynchronize(TimeStampedMessage msg) {
-		
-		   for(int i = 0; i < timestamp.length; i++)
-           {
-                   timestamp[i] = Math.max(timestamp[i], ((int[])msg.getTimeStamp())[i] );
-           }
-           timestamp[id]++;
-		
+
+		for (int i = 0; i < timestamp.length; i++) {
+			timestamp[i] = Math.max(timestamp[i],
+					((int[]) msg.getTimeStamp())[i]);
+		}
+		timestamp[id]++;
+
 	}
 
 	@Override
 	public boolean equal(int[] message1, int[] message2) {
-	       int i;
-           for(i = 0; i < message1.length; i++)
-           {
-                   if(message1[i] == message2[i])
-                           continue;
-                   break;
-           }
-           if(i != message1.length)
-                   return false;
-           else
-                   return true;
+		int i;
+		for (i = 0; i < message1.length; i++) {
+			if (message1[i] == message2[i])
+				continue;
+			break;
+		}
+		if (i != message1.length)
+			return false;
+		else
+			return true;
 	}
 
 }
 
-
-/*
-public class VectorClock implements Clock<int[], TimeStampedMessage>, java.io.Serializable
-{
-        private int size;
-        private int local_id;
-        private int[] timestamp;
-        public VectorClock(int size, int local_id)
-        {
-                this.size = size;
-                this.local_id = local_id;
-                this.timestamp = new int[size];
-        }
-        synchronized public int[] getTimeStamp(){return timestamp;}
-        
-        synchronized public int[] inc()
-        {
-                timestamp[local_id]++;
-                return Arrays.copyOf(timestamp, size);
-        }
-        synchronized public void syncWith(TimeStampedMessage t_msg)
-        {
-                for(int i = 0; i < timestamp.length; i++)
-                {
-                        timestamp[i] = Math.max(timestamp[i], ((int[])t_msg.getTimeStamp())[i]);
-                }
-                timestamp[local_id]++;
-        }
-
-        public boolean equal(int[] tm1, int[] tm2)
-        {
-                int i;
-                for(i = 0; i < tm1.length; i++)
-                {
-                        if(tm1[i] == tm2[i])
-                                continue;
-                        break;
-                }
-                if(i == tm1.length)
-                        return true;
-                else
-                        return false;
-        }
-
-        private boolean less_than_equal(int[] tm1, int[] tm2)
-        {
-                int i;
-                for(i = 0; i < tm1.length; i++)
-                {
-                        if(tm1[i] <= tm2[i])
-                                continue;
-                        break;
-                }
-                if(i == tm1.length)
-                        return true;
-                else
-                        return false;
-        }
-
-        private boolean less_than(int[] tm1, int[] tm2)
-        {
-                return (less_than_equal(tm1, tm2) && !equal(tm1, tm2));
-        }
-
-        public int compare(int[] tm1, int[] tm2)
-        {
-                if(less_than(tm1, tm2))
-                        return -1;
-                else if(less_than(tm2, tm1))
-                        return 1;
-                else   // tm1 == tm2 or tm1 || tm2
-                        return 0;
-        }
-}
-
- */
